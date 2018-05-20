2d9cbce5da0d252354dc2850c52d08e08f467252
Mojib Mohammad <mojib_monir@yahoo.com>
2018-05-20 13:11:30 -0700

    css style, logic changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,113 +1,174 @@
-$(document).ready(function() {
+var panel = $("#quiz-area");
 
-// Questions and answers array object.
-var  allQuestions = [
-  {
-    question: "Who is the strongest?",
-    choices: ["Superman","The Terminator","Waluigi, obviously","John Cina"],
-    correctAnswer: "John Cina"
-  },
-  {
-    question: "What is the best site ever created?",
-    choices: ["SitePoint","Simple Steps Code","Trick question; they're both the best","My site"],
-    correctAnswer: "SitePoint"
-  },
-  {
-    question: "How many states does USA has?",
-    choices: ["32","50","20","65"],
-    correctAnswer: "50"
-  }
-];
+// Question set
+var questions = [{
+  question: "What was the first full length CGI movie?",
+  answers: ["A Bug's Life", "Monsters Inc.", "Toy Story", "The Lion King"],
+  correctAnswer: "Toy Story"
+}, {
+  question: "Which of these is NOT a name of one of the Spice Girls?",
+  answers: ["Sporty Spice", "Fred Spice", "Scary Spice", "Posh Spice"],
+  correctAnswer: "Fred Spice"
+}, {
+  question: "Which NBA team won the most titles in the 90s?",
+  answers: ["New York Knicks", "Portland Trailblazers", "Los Angeles Lakers", "Chicago Bulls"],
+  correctAnswer: "Chicago Bulls"
+}, {
+  question: "Which group released the hit song, \"Smells Like Teen Spirit\"?",
+  answers: ["Nirvana", "Backstreet Boys", "The Offspring", "No Doubt"],
+  correctAnswer: "Nirvana"
+}, {
+  question: "Which popular Disney movie featured the song, \"Circle of Life\"?",
+  answers: ["Aladdin", "Hercules", "Mulan", "The Lion King"],
+  correctAnswer: "The Lion King"
+}, {
+  question: "Finish this line from the Fresh Prince of Bel-Air theme song: \"I whistled for a cab and when it came near, the license plate said...\"",
+  answers: ["Dice", "Mirror", "Fresh", "Cab"],
+  correctAnswer: "Fresh"
+}, {
+  question: "What was Doug's best friend's name?",
+  answers: ["Skeeter", "Mark", "Zach", "Cody"],
+  correctAnswer: "Skeeter"
+}, {
+  question: "What was the name of the principal at Bayside High in Saved By The Bell?",
+  answers: ["Mr.Zhou", "Mr.Driggers", "Mr.Belding", "Mr.Page"],
+  correctAnswer: "Mr.Belding"
+}];
 
+// Variable that will hold the setInterval
+var timer;
 
+var game = {
 
-// variables we need 
-var currentQuestion = 0;
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-var unanswered = 0;
+  correct: 0,
+  incorrect: 0,
+  counter: 120,
 
-
-// time is 1 minute
-var timer = 60;
-var time;
-
-function myTimer() {
-  timer--;
-  $("#timer").html("Time Remaining: " + timer);
-
-  if (timer === 0) {
-    clearInterval(time);
-    alert("Time Up!");
-  }
-
-}
-
-function setupOptions() {
-  $('#question').html(parseInt(currentQuestion) + 1 + ". " + allQuestions[currentQuestion].question);
-  var options = allQuestions[currentQuestion].choices;
-  var formHtml = '';
-for (var i = 0; i < options.length; i++) {
-  formHtml += '<div><input type="radio" name="option" value="' + i + '" id="option' + i + '"><label for="option' + i + '">' +
-  allQuestions[currentQuestion].choices[i] + '</label></div><br/>';
-
-} 
-  $('#form').html(formHtml);
-  $("#option0").prop('checked', true);
-};  
-// When the button is clicked we want our timer and quiz to start.
-  $("#startButton").on("click", function(event) {
-    event.preventDefault();
-    time = setInterval(myTimer, 1000); 
-
-  function checkAns() {
-    if ($("input[name=option]:checked").val() == allQuestions[currentQuestion].correctAnswer) {
-      correctAnswers++;
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
     }
-    else {
-      incorrectAnswers++;
+  },
+
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>120</span> Seconds</h2>");
+
+    $("#start").remove();
+
+    for (var i = 0; i < questions.length; i++) {
+      panel.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append("<input type='radio' name='question-" + i +
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
     }
-  };
-  
-    setupOptions();
-  
-    $("#next").click(function() {
-      event.preventDefault();
-      checkAns();
-      currentQuestion++;
-      $(function() {
-        $("#progressbar").progressbar({
-          value: currentQuestion
-        });
-      });
-      if (currentQuestion < allQuestions.length) {
-        setupOptions();
-        if (currentQuestion == allQuestions.length - 1) {          
-  
-        };
-        $('#submit').click(function() {
-          $("#submit").html("Correct Answers: " + correctAnswers + "<br>" + "Incorrect Answers: " + incorrectAnswers);
-            $("#result").html("You correctly answered " + correctAnswers + " out of " + currentquestion + " questions! ").hide();
-            $("#submit").fadeIn(1500);
-        });
-      };
+
+    panel.append("<button id='done'>Done</button>");
+  },
+
+  done: function() {
+
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
     });
-  });
+
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-4']:checked"), function() {
+      if ($(this).val() === questions[4].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-5']:checked"), function() {
+      if ($(this).val() === questions[5].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-6']:checked"), function() {
+      if ($(this).val() === questions[6].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-7']:checked"), function() {
+      if ($(this).val() === questions[7].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+
+  },
+
+  result: function() {
+
+    clearInterval(timer);
+
+    $("#sub-wrapper h2").remove();
+
+    panel.html("<h2>All Done!</h2>");
+    panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
+};
+
+// CLICK EVENTS
+
+$(document).on("click", "#start", function() {
+  game.start();
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).on("click", "#done", function() {
+  game.done();
+});
